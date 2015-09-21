78afcac57838a2da2faa03330f5c0a0bc3b3d4cc
coperd <lhcwhu@gmail.com>
2015-09-20 21:09:34 -0500

    new vundle-managed conf

---
+++ install_basic_vimrc.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
+vim +PluginInstall +qall
+echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
