c364e06ac6c07dbdd40ef775271a631244bee84c
Huaicheng Li <lhcwhu@gmail.com>
2025-06-17 14:59:12 -0400

    Update install script

--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -2,15 +2,15 @@
 
 echo 'set runtimepath+=~/.vim_runtime
 
-source ~/.vim_runtime/vimrcs/plug_config.vim
+source ~/.vim_runtime/vimrcs/plug.vim
 source ~/.vim_runtime/vimrcs/basic.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+"source ~/.vim_runtime/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
 # install plugins
-vim +PluginInstall +qall
+vim +PlugInstall +qall
 
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
